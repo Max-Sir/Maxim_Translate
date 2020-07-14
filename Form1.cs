a6dff6b8ed982bc9551bf6a01a66cb31f8568145
Maxim <syromolotov.maks@mail.ru>
2020-07-14 21:44:42 +0300

    Maxim Translate v 1.0 07/14/2020 En-Ru Translation only

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+        YandexTranslator yt;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            yt = new YandexTranslator();
+        }
+
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            string lang;
+
+            if (radioButton1.Checked == true)
+            {
+                lang = "ru-en";
+            }
+            else
+            {
+                lang = "en-ru";
+            }
+
+            textBox2.Text = yt.Translate(textBox1.Text, lang);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                VisitLink();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+        private void VisitLink()
+        {
+            // Change the color of the link text by setting LinkVisited
+            // to true.
+            linkLabel1.LinkVisited = true;
+            //Call the Process.Start method to open the default browser
+            //with a URL:
+            System.Diagnostics.Process.Start("https://github.com/Max-Sir");
+        }
+    }
+}
---
+++ YandexTranslator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace Translator
+{
+    class YandexTranslator
+    {
+        public string Translate(string s, string lang)
+        {
+            if (s.Length > 0)
+            {
+                WebRequest request = WebRequest.Create("https://translate.yandex.net/api/v1.5/tr.json/translate?"
+                                                       + "key=trnsl.1.1.20190227T075339Z.1b02a9ab6d4a47cc.f37d50831b51374ee600fd6aa0259419fd7ecd97"
+                                                       + "&text=" + s
+                                                       + "&lang=" + lang);
+
+                WebResponse response = request.GetResponse();
+
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    string line;
+
+                    if ((line = stream.ReadLine()) != null)
+                    {
+                        Translation translation = JsonConvert.DeserializeObject<Translation>(line);
+
+                        s = "";
+
+                        foreach (string str in translation.text)
+                        {
+                            s += str;
+                        }
+                    }
+                }
+
+                return s;
+            }
+            else
+                return "";
+        }
+    }
+
+    class Translation
+    {
+        public string code { get; set; }
+        public string lang { get; set; }
+        public string[] text { get; set; }
+    }
+}
